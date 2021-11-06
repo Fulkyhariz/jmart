9be4ddd0a6a3f808a673be8be987d3c5391f6425
Fulky Hariz <fulky.hariz@gmail.com>
2021-11-06 11:27:22 +0700

    memperbaiki filter dan juga read

--- src/fulkyJmartRK/Jmart.java
+++ src/fulkyJmartRK/Jmart.java
@@ -24,42 +24,58 @@ public class Jmart
     }*/
     public static void main(String[] args)
     {
-        System.out.println("account id:" + new Account(null, null, null, -1).id);
+        /*System.out.println("account id:" + new Account(null, null, null, -1).id);
         System.out.println("account id:" + new Account(null, null, null, -1).id);
         System.out.println("account id:" + new Account(null, null, null, -1).id);
 
         System.out.println("payment id:" + new Payment(-1, -1, -1, null).id);
         System.out.println("payment id:" + new Payment(-1, -1, -1, null).id);
-        System.out.println("payment id:" + new Payment(-1, -1, -1, null).id);
+        System.out.println("payment id:" + new Payment(-1, -1, -1, null).id);*/
 
-        /*String filepath = "C:/Users/fulky/Documents/Akademik/5th Term/OOP/Prak/Jmart/jmart/city.json";
-        Gson gson = new Gson();
         try{
-            BufferedReader br = new BufferedReader((new FileReader(filepath)));
-            Country input = gson.fromJson(br, Country.class);
-            System.out.println("name: " + input.name);
-            System.out.println("population: " + input.population);
-            System.out.println("states: ");
-            input.listOfStates.forEach(state -> System.out.println(state));
-        }catch (IOException e){
-            e.printStackTrace();
-        }*/
+            List<Product> list = read("C:/Users/fulky/Documents/Akademik/5th Term/OOP/Prak/[Modul 6] Case Study - Resource-20211106/randomProductList.json");
+            List<Product> filtered = filterByPrice(list, 13000.0, 15000.0);
+            filtered.forEach(product -> System.out.println(product.price));
+        }catch (Throwable t){
+            t.printStackTrace();
+        }
     }
     public static List<Product> read (String filepath){
-        Gson gson = new Gson();
         List<Product> temp = new ArrayList<>();
         try{
-            JsonReader jsonReader = new JsonReader((new FileReader(filepath)));
-            temp = gson.fromJson(jsonReader, Product.class);
+            Gson gson = new Gson();
+            JsonReader reader = new JsonReader(new FileReader(filepath));
+            reader.beginArray();
+            while(reader.hasNext()){
+                temp.add(gson.fromJson(reader, Product.class));
+            }
         }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
             e.printStackTrace();
         }
         return temp;
     }
     public static List<Product> filterByCategory (List<Product> list, ProductCategory category){
-        return null;
+        List<Product> filtered = new ArrayList<>();
+        for(Product temp : list){
+            if(temp.category == category){
+                filtered.add(temp);
+            }
+        }
+        return filtered;
     }
     public static List<Product> filterByPrice (List<Product> list, double minPrice, double maxPrice){
-        return null;
+        List<Product> filtered = new ArrayList<>();
+        for(Product temp : list){
+            if(minPrice == 0.0 && maxPrice >= temp.price){
+                filtered.add(temp);
+            }else if(maxPrice == 0.0 && minPrice <= temp.price){
+                filtered.add(temp);
+            }else if(maxPrice >= temp.price && minPrice <= temp.price){
+                filtered.add(temp);
+            }
+        }
+        return filtered;
     }
 }
