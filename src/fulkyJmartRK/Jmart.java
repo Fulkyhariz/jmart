1e82efebda9635c51297a2953bfc9c268c16f76a
Fulky Hariz <fulky.hariz@gmail.com>
2021-11-08 20:55:18 +0700

    post test modul 6

--- src/fulkyJmartRK/Jmart.java
+++ src/fulkyJmartRK/Jmart.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.List;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
+import java.util.stream.Collectors;
 
 /**
  * Write a description of class Jmart here.
@@ -34,25 +35,19 @@ public class Jmart
 
         try{
             List<Product> list = read("C:/Users/fulky/Documents/Akademik/5th Term/OOP/Prak/[Modul 6] Case Study - Resource-20211106/randomProductList.json");
-            List<Product> filtered = filterByPrice(list, 13000.0, 15000.0);
-            filtered.forEach(product -> System.out.println(product.price));
+            List<Product> filtered = filterByName(list, "gtx", 1, 4);
+            filtered.forEach(product -> System.out.println(product.name));
         }catch (Throwable t){
             t.printStackTrace();
         }
     }
-    public static List<Product> read (String filepath){
+    public static List<Product> read (String filepath) throws FileNotFoundException, IOException{
         List<Product> temp = new ArrayList<>();
-        try{
-            Gson gson = new Gson();
-            JsonReader reader = new JsonReader(new FileReader(filepath));
-            reader.beginArray();
-            while(reader.hasNext()){
-                temp.add(gson.fromJson(reader, Product.class));
-            }
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
-        }catch (IOException e){
-            e.printStackTrace();
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new FileReader(filepath));
+        reader.beginArray();
+        while(reader.hasNext()){
+        temp.add(gson.fromJson(reader, Product.class));
         }
         return temp;
     }
@@ -77,5 +72,48 @@ public class Jmart
             }
         }
         return filtered;
+    }
+    public static List<Product> filterByAccountId(List<Product> list, int accountId, int page, int pageSize){
+/*        List<Product> temp = new ArrayList<>();
+        for(Product value : list){
+            if (value.id == accountId){
+                temp.add(value);
+            }
+        }*/
+        Predicate<Product> pred= (tmp -> (tmp.accountId == accountId));
+        return paginate(list, page, pageSize, pred);
+    }
+    public static List<Product> filterByName(List<Product> list, String search, int page, int pageSize){
+/*        List<Product> temp = new ArrayList<>();
+        for(Product value : list){
+            if (value.name == search){
+                temp.add(value);
+            }
+        }*/
+        Predicate<Product> pred = (tmp -> (tmp.name.toLowerCase().contains(search.toLowerCase())));
+        return paginate(list, page, pageSize, pred);
+    }
+    private static List<Product> paginate(List<Product> list, int page, int pageSize, Predicate<Product> pred){
+        List<Product> temp = new ArrayList<>();
+        int counter = 0;
+        List<Product> paginated = new ArrayList<>();
+        for(Product value : list){
+            if(pred.predicate(value)){
+                temp.add(value);
+            }
+        }
+        for (int i = 0; i < temp.size() ; i++){
+            if( (i%pageSize) == 0){
+                counter += 1;
+                if (counter == page){
+                    counter = pageSize * page;
+                    for (int j = 0 ; j < pageSize ; j++){
+                        paginated.add(temp.get(counter));
+                        counter += 1;
+                    }
+                }
+            }
+        }
+        return paginated;
     }
 }
